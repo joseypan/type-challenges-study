b0af206dd1f71c143ae40779ee86ddd86f1e9751
panxumin <joseypan@163.com>
2022-06-14 13:12:16 +0800

    fix: 完成essy-first

---
+++ questions/00014-easy-first/thinking.js
@@ -0,0 +1,7 @@
+//【分析】根据题目的要求，我们是需要返回数组的第一项的，同时也要考虑到数组长度为空时返回never类型
+//【疑问】
+//1、什么是never类型？如何表示出never类型？
+//never 是任何类型的⼦类型，也就是说 never 可以赋值给任何类型。
+//https://www.zhihu.com/question/354601204/answer/888551021
+//2、如何判断数组长度呢？使用到extends.如何理解ts中的extends呢？
+//https://blog.csdn.net/qq_34998786/article/details/120300361
