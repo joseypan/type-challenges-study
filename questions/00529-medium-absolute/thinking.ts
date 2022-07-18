f87aa2697ef25a6774c10f4c3d3b300ba0565d54
panxumin <joseypan@163.com>
2022-07-18 13:41:32 +0800

    feat: 添加数字n在js中的含义解释

--- questions/00529-medium-absolute/thinking.ts
+++ questions/00529-medium-absolute/thinking.ts
@@ -2,6 +2,7 @@
 // 1、按照测试用例可知，需要将传入的数字或字符串进行取绝对值操作，所以就是匹配-？
 // 使用`${T}`是一开始就都将类型转成字符串类型，统一处理
 // 999_9n代表什么？
+// n的作用：js中用于显示大数字的定义 普通的数字超过一定数值会丢失精度 这种方式是专门做大数字的运算用的
 type CustomAbsolute<T extends number | string | bigint> =
   `${T}` extends `-${infer x}` ? `${x}` : `${T}`
 type TestAbsolute = CustomAbsolute<-9_999n>
