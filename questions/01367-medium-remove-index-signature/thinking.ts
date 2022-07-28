1959696d2b4a8eacacc2761687ca893e2f997f7f
joseypan <joseypan@163.com>
2022-07-28 22:03:44 +0800

    fix: 完成medium-remove-index-signature

--- questions/01367-medium-remove-index-signature/thinking.ts
+++ questions/01367-medium-remove-index-signature/thinking.ts
@@ -2,3 +2,13 @@
 // 1、根据测试用例，其实不太清楚这个如何处理
 // 2、查看题目相关说明：exclude the index signature from object types.
 // 排除对象是索引符号的类型，如何理解这个索引号
+// https://basarat.gitbook.io/typescript/type-system/index-signatures
+type CustomRemoveIndexSignature<T> = {
+  [K in keyof T as string extends K
+    ? never
+    : number extends K
+    ? never
+    : symbol extends K
+    ? never
+    : K]: T[K]
+}
