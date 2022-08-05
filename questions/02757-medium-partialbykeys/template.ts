e43446306b6b1bfe1e8a3ecbffdc2b7a18f1802a
panxumin <joseypan@163.com>
2022-08-05 09:54:53 +0800

    fix: 修改medium-requiredbykeys

--- questions/02757-medium-partialbykeys/template.ts
+++ questions/02757-medium-partialbykeys/template.ts
@@ -1,3 +1,9 @@
-type PartialByKeys<T, K extends keyof T> = T & {
-  [key in keyof T as key extends K ? key : never]?: T[key]
-}
+// type PartialByKeys<T, K extends keyof T> = T & {
+//   [key in keyof T as key extends K ? key : never]?: T[key]
+// }
+
+type PartialByKeys<T, K = keyof T> = Merge<
+  { [key in keyof T as key extends K ? never : key]: T[key] } & {
+    [key in keyof T as key extends K ? key : never]?: T[key]
+  }
+>
--- questions/02757-medium-partialbykeys/thinking.ts
+++ questions/02757-medium-partialbykeys/thinking.ts
@@ -4,9 +4,15 @@
 // 3、如何将一个对象的某一项变成可选参数呢，可以使用官方的partial吗？
 // 4、是否可以利用对象的合并特性，将处理后的对象与原对象进行合并处理(好像是可以通过测试用例的)
 
-type CustomPartialByKeys<T, K extends keyof T> = T & {
-  [key in keyof T as key extends K ? key : never]?: T[key]
-}
+type CustomPartialByKeys<T, K extends keyof T> = Merge<
+  { [key in keyof T as key extends K ? never : key]: T[key] } & {
+    [key in keyof T as key extends K ? key : never]?: T[key]
+  }
+>
+// T & {
+//   [key in keyof T as key extends K ? key : never]?: T[key]
+// }
+
 interface User {
   name: string
   age: number
