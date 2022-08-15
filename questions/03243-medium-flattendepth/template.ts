865c4f64a7c3eb0bedd0cef8d9e0652c90c7cc49
panxumin <joseypan@163.com>
2022-08-15 10:31:06 +0800

    fix: 更新实现逻辑

--- questions/03243-medium-flattendepth/template.ts
+++ questions/03243-medium-flattendepth/template.ts
@@ -1,9 +1,20 @@
+type FlattenOnce<T extends any[]> = T extends [infer F, ...infer R]
+  ? [...(F extends any[] ? F : [F]), ...FlattenOnce<R>]
+  : []
+
+// T 是否平铺的数组
+type IsFlatten<T extends any[]> = T extends [infer F, ...infer R]
+  ? F extends any[]
+    ? false
+    : IsFlatten<R>
+  : true
+
 type FlattenDepth<
-  A extends any[],
-  T extends number = 1,
-  U extends number[] = [],
-> = U['length'] extends T
-  ? A
-  : A extends any[]
-  ? FlattenDepth<Flatten<A>, T, [...U, 1]>
-  : A
+  T extends any[],
+  D extends number = 1,
+  Reduce extends any[] = [],
+> = IsFlatten<T> extends true
+  ? T
+  : Reduce['length'] extends D
+  ? T
+  : FlattenDepth<FlattenOnce<T>, D, [any, ...Reduce]>
